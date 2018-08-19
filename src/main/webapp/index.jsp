b7956851fd6f98a866bf2eb503ea19388d8a176d
Harsha <harshavardhan.chakka@gmail.com>
2018-08-19 23:01:08 +0530

    WebProject

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Project to add</title>
+</head>
+<body>
+<h3>Enter the values to find sum </h3>
+<form action="UnitTest">
+
+	<input type="text" name="n1">
+	<input type="text" name="n2">
+	<input type="submit" name="sumbit" value="Calculate sum">
+	
+</form>
+</body>
+</html> 
---
+++ src/test/java/testpkg/Testadd.java
@@ -0,0 +1,59 @@
+package testpkg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.Nissan.webproject.UnitTest;
+
+public class Testadd {
+
+	
+	@Test
+	public void int_ast_Test() {
+		UnitTest f=new UnitTest();
+		String num1="3";
+		String num2="!";
+		assertEquals("please type int or float",f.Add(num1,num2));
+		
+	}
+	@Test
+	public void float_3_12__4_13_Test() {
+		UnitTest f=new UnitTest();
+		String num1="3.12";
+		String num2="4.13";
+		assertEquals("7.25",f.Add(num1,num2));
+		
+	}
+	@Test
+	public void int_at_Test() {
+		UnitTest f=new UnitTest();
+		String num1="@";
+		String num2="4.13";
+		assertEquals("please type int or float",f.Add(num1,num2));
+		
+	}
+	@Test
+	public void int_0_9999__1_Test() {
+		UnitTest f=new UnitTest();
+		String num1="0.9999";
+		String num2="1";
+		assertEquals("1.9999",f.Add(num1,num2));
+		
+	}
+	@Test
+	public void int__Test() {
+		UnitTest f=new UnitTest();
+		String num1="_";
+		String num2="1";
+		assertEquals("please type int or float",f.Add(num1,num2));
+		
+	}
+	@Test
+	public void float_0_001__0_000001_Test() {
+		UnitTest f=new UnitTest();
+		String num1="0.001";
+		String num2="0.000001";
+		assertEquals("0.001001",f.Add(num1,num2)); 
+	}
+}
